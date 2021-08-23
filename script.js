ee25b15510e0c5b4c0fb2f49246804e1acde6232
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-08-23 12:23:07 +0200

    Created working game function. Fix the problem with the computer choice being assined only once. Still can make the code cleaner.

--- script.js
+++ script.js
@@ -1,26 +1,73 @@
-const computerSelection = computerPlay();
-function computerPlay(string) {
-    let randomNumber = Math.floor(Math.random() * 3 + 1);   
-    if (randomNumber === 1) {
-        string = 'Rock';
-        return string;
-    } else if (randomNumber === 2) {
-        string = 'Paper';
-        return string;
-    } else {
-        string = 'Scissors';
-        return string;
-    }
-}
-const playerSelection = 'Rock';  /*  NEEDS TO NE CASE INSENTIVITE*/
-function gamePlay(playerSelection, computerSelection) {
+let playerWin = 0;   //Variable to track player wins
+let computerWin = 0; //Variable to track computer wins
+        function computerPlay(string) {                         //
+            let randomNumber = Math.random();   
+            if (randomNumber <= 1/3) {
+                string = 'rock';
+                return string;
+            } else if (randomNumber <= 2/3) {
+                string = 'paper';
+                return string;
+            } else {
+                string = 'scissors';
+                return string;
+            }
+        }
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-    console.log(`TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`);
+    return `TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`;
     } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') ||
     (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
-    console.log(`YOU WON! ${playerSelection} beats ${computerSelection}`);
+        playerWin += 1;
+    return `YOU WON! ${playerSelection} beats ${computerSelection}`;
     } else {
-    console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}`);
+    computerWin += 1;
+    return `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
+}  
+}
+/* CODE WITHOUT LOOP
+function game() {
+    playerSelection = prompt('Choose between Rock, Paper, and Scissors!');
+    computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    layerSelection = prompt('Choose between Rock, Paper, and Scissors!');
+    computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    layerSelection = prompt('Choose between Rock, Paper, and Scissors!');
+    computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    layerSelection = prompt('Choose between Rock, Paper, and Scissors!');
+    computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    playerSelection = prompt('Choose between Rock, Paper, and Scissors!');
+    computerSelection = computerPlay(); 
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`After 5 rounds you have ${playerWin} wins and computer has ${computerWin}` );
+    if (playerWin > computerWin) {
+        console.log('You are the winner!');
+    } else if (playerWin < computerWin) {
+        console.log('Computer is the winnner!');
+    } else if (playerWin === computerWin) {
+        console.log('IT\'S A TIE!!!');
+    } else {
+        console.log('OH NOOO> SOMETHING DID BROKE!!!');
+    }
+}*/
+function game() {
+    for (let i = 0; i< 5; i++) {
+        let playerSelection = prompt('Choose between Rock, Paper, and Scissors!').toLowerCase();
+        computerSelection = computerPlay(); 
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(`After 5 rounds you have ${playerWin} wins and computer has ${computerWin}` );
+    if (playerWin > computerWin) {
+        console.log('You are the winner!');
+    } else if (playerWin < computerWin) {
+        console.log('Computer is the winnner!');
+    } else if (playerWin === computerWin) {
+        console.log('IT\'S A TIE!!!');
+    } else {
+        console.log('OH NOOO> SOMETHING DID BROKE!!!');
     }
 }
-console.log(gamePlay(playerSelection, computerSelection));
+game();
