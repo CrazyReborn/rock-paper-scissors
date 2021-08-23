ce6073a62db53a937f5396d1f64b1d0a08961b1f
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-08-23 14:21:43 +0200

    Made code a bit cleaner.

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 let playerWin = 0;   
 let computerWin = 0; 
-        function computerPlay(string) {                         
-            let randomNumber = Math.random();   
-            if (randomNumber <= 1/3) {
-                string = 'rock';
-                return string;
-            } else if (randomNumber <= 2/3) {
-                string = 'paper';
-                return string;
-            } else {
-                string = 'scissors';
-                return string;
-            }
-        }
+function computerPlay(string) {                         
+    let randomNumber = Math.random();   
+    if (randomNumber <= 1/3) {
+        string = 'rock';
+        return string;
+    } else if (randomNumber <= 2/3) {
+        string = 'paper';
+        return string;
+    } else {
+        string = 'scissors';
+        return string;
+    }
+}
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-    return `TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`;
+        playerWin += 1;
+        computerWin += 1;
+        return `TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`;
     } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') ||
     (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
         playerWin += 1;
-    return `YOU WON! ${playerSelection} beats ${computerSelection}`;
+        return `YOU WON! ${playerSelection} beats ${computerSelection}`;
     } else {
-    computerWin += 1;
-    return `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
-}  
+        computerWin += 1;
+        return `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
+    }  
 }
 function game() {
     for (let i = 0; i< 5; i++) {
