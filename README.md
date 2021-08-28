db4616e9b58e22ef9fb95f8269c7a73e3b57dd72
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-08-28 18:20:37 +0200

    First right commit.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a simple console game from The Odin Project;
+Basic design and funcionality;
--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let playerWin = 0;   
 let computerWin = 0; 
+const buttonRock = document.getElementById('rock');
+const buttonPaper = document.getElementById('paper');
+const buttonScissors = document.getElementById('scissors');
+const divResults = document.getElementById('results');
+const para = document.createElement('p');
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+let gameRounds = 0;
+
+
 function computerPlay(string) {                         
     let randomNumber = Math.random();   
     if (randomNumber <= 1/3) {
@@ -13,20 +23,63 @@ function computerPlay(string) {
         return string;
     }
 }
-function playRound(playerSelection, computerSelection) {
+
+function playGame(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        para.textContent = `Round ${gameRounds + 1}: TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`;
+        divResults.appendChild(para);
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')) {
         playerWin += 1;
-        computerWin += 1;
-        return `TIE! You chose ${playerSelection} and computer chose ${computerSelection}!`;
-    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') ||
-    (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
-        playerWin += 1;
-        return `YOU WON! ${playerSelection} beats ${computerSelection}`;
+        para.textContent = `Round ${gameRounds + 1}: YOU WON! ${playerSelection} beats ${computerSelection}!`;
+        divResults.appendChild(para);
     } else {
         computerWin += 1;
-        return `YOU LOSE! ${computerSelection} beats ${playerSelection}`;
-    }  
+        para.textContent = `Round ${gameRounds +1}: YOU LOSE! ${computerSelection} beats ${playerSelection}!`;
+        divResults.appendChild(para);
+    }
+    playerScore.textContent = 'Player score: ' + playerWin;
+    computerScore.textContent = 'Computer score: ' +computerWin;
+    gameRounds ++;
+    if (gameRounds == 5) {
+        if (playerWin > computerWin) {
+            para.textContent = 'YOU WIN!';
+        } else if (playerWin < computerWin) {
+            para.textContent = 'COMPUTER WINS!';
+        } else {
+            para.textContent = 'TIE!';
+        }
+        playerWin = 0;
+        computerWin = 0;
+        gameRounds = 0;
+        playerScore.textContent = 'Player score: ' + 0;
+        computerScore.textContent = 'Computer score: ' + 0; 
+    }
 }
+
+buttonRock.addEventListener('click', function(e) {
+    let computerSelection = computerPlay();
+    playGame('rock', computerSelection);
+});
+
+buttonPaper.addEventListener('click', function(e) {
+    let computerSelection = computerPlay();
+    playGame('paper', computerSelection);
+    
+});
+
+buttonScissors.addEventListener('click', function(e) {
+    let computerSelection = computerPlay();
+    playGame('scissors', computerSelection);
+    
+});
+
+
+/*
+buttonPaper.addEventListener('click', playRound('Paper', computerSelection));
+buttonScissors.addEventListener('click', playRound('Scissors', computerSelection));
+
+/*
 function game() {
     for (let i = 0; i< 5; i++) {
         let playerSelection = prompt('Choose between Rock, Paper, and Scissors!').toLowerCase();
@@ -45,3 +98,4 @@ function game() {
     }
 }
 game();
+*/
